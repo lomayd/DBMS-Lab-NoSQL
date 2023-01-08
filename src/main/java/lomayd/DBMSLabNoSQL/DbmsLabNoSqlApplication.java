367b0fa110d7ac35a8bd824e2d974578f57c778f
lomayd <jinho10167@gmail.com>
2023-01-08 13:00:50 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabNoSQL/DbmsLabNoSqlApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/DBMSLabNoSQL/DbmsLabNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
